a4be029bce1d0fe695cc7b0447f3886af148b136
mihai <mihai.scarlat@smartcontrol.ro>
2025-12-03 17:42:04 +0200

    delete

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Maven Group heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Maven Group/i);
-  expect(headingElement).toBeInTheDocument();
-});
